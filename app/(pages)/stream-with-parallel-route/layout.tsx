201b92bd2afe6f916f5af820086527e5ae556210
Mehrdad <mnourib13@gmail.com>
2025-08-17 22:49:51 +0330

    Add static page to layout of parallel

--- app/(pages)/stream-with-parallel-route/layout.tsx
+++ app/(pages)/stream-with-parallel-route/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
+import Static from '../stream-with-suspense/sections/static';
 
 export default function layout({ children, parallel }: { children: ReactNode, parallel: ReactNode }) {
     return (
         <div className='flex flex-col gap-6'>
+            <Static />
             {children}
             <div className='p-4 bg-cyan-50 border border-cyan-500 rounded-2xl'>
                 <p className='text-cyan-700 mb-4 font-semibold'>Parallel Route</p>
