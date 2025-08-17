bbe27cd6ba238c93ba3576976384f69de3db0fb0
Mehrdad <mnourib13@gmail.com>
2025-08-17 22:47:41 +0330

    Bring back revaldiation parallel

--- app/(pages)/stream-with-parallel-route/@parallel/page.tsx
+++ app/(pages)/stream-with-parallel-route/@parallel/page.tsx
@@ -1,6 +1,5 @@
 import RevalidateEvery from '../../stream-with-suspense/sections/RevalidateEvry';
 
 export default function page() {
-    // return <RevalidateEvery revalidate={1} />
-    return null
+    return <RevalidateEvery revalidate={1} />
 }
