e59f231eee6b18ebaf40537c3fb5cabf62f432a2
Mehrdad <mnourib13@gmail.com>
2025-08-17 22:18:25 +0330

    Add revalidate time to root page

--- app/(pages)/stream-with-suspense/page.tsx
+++ app/(pages)/stream-with-suspense/page.tsx
@@ -3,6 +3,9 @@ import { Suspense } from 'react';
 import Static from './sections/static';
 import RevalidateEvery from './sections/RevalidateEvry';
 
+
+export const revalidate = 60;
+
 export default async function page() {
 
     return (
@@ -10,9 +13,13 @@ export default async function page() {
             <Suspense>
                 <Static />
             </Suspense>
-            {/* <Suspense>
+            <div className='bg-cyan-50 text-lg text-cyan-700 rounded-xl p-4'>
+                The section above  has no validation time but it revalidates every 1 minute as the below component determines the
+                revalidation of page as the minimum revalidation used in the page
+            </div>
+            <Suspense>
                 <RevalidateEvery revalidate={1} />
-            </Suspense> */}
+            </Suspense>
         </div>
     );
 }
