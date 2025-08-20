113a8f5aaf2a038d520c8cf4a9e5675ac25a28b0
Mehrdad <mnourib13@gmail.com>
2025-08-20 22:36:48 +0330

    Check for merge

--- app/(pages)/revalidate-on-demand/page.tsx
+++ app/(pages)/revalidate-on-demand/page.tsx
@@ -19,6 +19,7 @@ export default async function page() {
                 <b className='ml-1'>revalidatePath</b> and see how each section updates differently in real
                 time.
             </NoteBox>
+            
             <NoteBox>
                 <div className='flex items-center gap-2'>
                     <p>
