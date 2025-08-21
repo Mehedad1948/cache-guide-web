54a775d314dd55969efac93ed5ea8ad0297b6a81
Mehrdad <mnourib13@gmail.com>
2025-08-21 23:56:58 +0330

    Add bested force cacehde

--- app/(pages)/nested-server-components/page.tsx
+++ app/(pages)/nested-server-components/page.tsx
@@ -4,9 +4,10 @@ import Static from '../stream-with-suspense/sections/static';
 export default async function page() {
     return (
         <div>
-            <h1 className='p-4 bg-cyan-50 mb-4 font-semibold text-cyan-700 rounded-xl'>A Permanent Cached Section Wrapped in a Revalidation Component</h1>
+            <h1 className='p-4 bg-cyan-50 flex flex-col gap-5 mb-4 font-semibold text-cyan-700 rounded-xl'>A Permanent Cached Section Wrapped in a Revalidation Component</h1>
             <RevalidateEvery revalidate={1}>
                 <Static />
+                <Static cache='force-cache' />
             </RevalidateEvery>
         </div>
     );
