6e18bb1f3723c740a897b1847e11e969eaf842ac
Mehrdad <mnourib13@gmail.com>
2025-08-22 09:16:36 +0330

    Add nested force cached uinque

--- app/(pages)/nested-server-components/page.tsx
+++ app/(pages)/nested-server-components/page.tsx
@@ -6,8 +6,8 @@ export default async function page() {
         <div>
             <h1 className='p-4 bg-cyan-50 flex flex-col gap-5 mb-4 font-semibold text-cyan-700 rounded-xl'>A Permanent Cached Section Wrapped in a Revalidation Component</h1>
             <RevalidateEvery revalidate={1}>
-                <Static />
-                <Static cache='force-cache' />
+                <Static identifier='nested-default-cache' />
+                <Static identifier='nested-force-cache' cache='force-cache' />
             </RevalidateEvery>
         </div>
     );
