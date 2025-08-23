829043256ebb1240e1bdf3dee65d9d2c5b61045c
Mehrdad <mnourib13@gmail.com>
2025-08-23 17:04:56 +0330

    Fix: build error

--- app/(pages)/partial-prerendering/not-activated/page.tsx
+++ app/(pages)/partial-prerendering/not-activated/page.tsx
@@ -81,7 +81,7 @@ async function Stream() {
 
 const shimmer = `relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent`;
 
-export function RecommendedProductsSkeleton() {
+function RecommendedProductsSkeleton() {
     return (
         <div className="space-y-6 pb-[5px]">
             <div className="space-y-2">
